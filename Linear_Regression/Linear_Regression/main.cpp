00b65a4221a636e9fa7c7aeef19f8425813d2e5d
AlejandroT23 <atoledo1@carthage.edu>
2025-11-06 19:43:50 -0600

    Created first files

---
+++ Linear_Regression/Linear_Regression/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Linear_Regression
+//
+//  Created by Alejandro Toledo on 11/6/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
